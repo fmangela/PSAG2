b9f5110dd4ec9bc4153b0ee984225302ed45e8d1
fmangela <523240830@qq.com>
2024-10-15 08:42:53 +0800

    重构Button1Operation类，引入Operation基类
    - 将Button1Operation类继承自新的Operation基类
    - 优化Button1Operation类的结构和方法
    - 在Operation基类中添加send_message方法，作为后续扩展的基础
    -调整项目配置，包括IDE设置和模块依赖

--- addons/operator/button_operation.py
+++ addons/operator/button_operation.py
@@ -1,26 +1,48 @@
 from PySide6.QtWidgets import QWidget
+from operation import Operation
 
 
-class Button1Operation:
+class Button1Operation(Operation):
     def __init__(self, ui: QWidget):
-        self.ui = ui
+        super().__init__(ui)
 
+        # 定义类内变量
+        self.input_cal_min_value = None
+        self.input_cal_max_value = None
+        self.tf_plus = None
+        self.tf_minus = None
+        self.tf_compare = None
+        self.tf_multi = None
+        self.tf_division = None
+        self.tf_remainder = None
+        self.input_amount = None
+
+    @self.lock_button_1()
     def button_1_operation(self):
         # 首先冻结该按钮，不能多次按下
         self.ui.button_generate_1.setEnabled(False)
         # 将输出文本清空内容
         self.ui.output_text.setText("")
-        # 获取输入值
-        input_cal_min_value = self.ui.input_range_min_num.text()
-        input_cal_max_value = self.ui.input_range_max_num.text()
-        tf_plus = self.ui.checkbox_tf_plus.isChecked()
-        tf_minus = self.ui.checkbox_tf_minus.isChecked()
-        tf_compare = self.ui.checkbox_tf_compare.isChecked()
-        tf_multi = self.ui.checkbox_tf_multi.isChecked()
-        tf_division = self.ui.checkbox_tf_division.isChecked()
-        tf_remainder = self.ui.checkbox_tf_remainder.isChecked()
-        input_amount = self.ui.input_questions_num.text()
-        # 检查输入是否合法
+
 
 
         pass
+
+    def get_parameters(self):
+        # 获取输入值
+        self.input_cal_min_value = self.ui.input_range_min_num.text()
+        self.input_cal_max_value = self.ui.input_range_max_num.text()
+        self.tf_plus = self.ui.checkbox_tf_plus.isChecked()
+        self.tf_minus = self.ui.checkbox_tf_minus.isChecked()
+        self.tf_compare = self.ui.checkbox_tf_compare.isChecked()
+        self.tf_multi = self.ui.checkbox_tf_multi.isChecked()
+        self.tf_division = self.ui.checkbox_tf_division.isChecked()
+        self.tf_remainder = self.ui.checkbox_tf_remainder.isChecked()
+        self.input_amount = self.ui.input_questions_num.text()
+        # 检查输入是否合法
+        pass
+
+    def lock_button_1(self, op_function, *args, **kwargs):
+        self.ui.button_generate_1.setEnabled(False)
+        op_function(*args, **kwargs)
+        self.ui.button_generate_1.setEnabled(True)
---
+++ addons/operator/operation.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import QWidget
+
+
+class Operation:
+    def __init__(self, ui: QWidget):
+        self.ui = ui
+
+    def send_message(self, message):
+        pass
